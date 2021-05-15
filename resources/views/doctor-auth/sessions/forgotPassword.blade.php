a8ce6dfe0da683ebc54618b2ca3d31a9ea24c686
Ahmed-Ibrahim99 <ahmedmaghraby1999@gmail.com>
2021-05-15 17:16:02 +0200

    added forgotPassword to doctor views

--- resources/views/doctor-auth/sessions/forgotPassword.blade.php
+++ resources/views/doctor-auth/sessions/forgotPassword.blade.php
@@ -1,10 +1,20 @@
-<title> Forgot Your Password? </title>
-<link rel="stylesheet" type="text/css" href="{{ asset('css/styleforgot.css') }}">
-<form class="box1" action="{{ route('session.restore') }}" method="post">
-    {{ csrf_field() }}
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
 
-    <h1> Forgot password </h1>
-    <p class="message"> Enter your email address to reset your password. You may need to check your email </p>
-    <input type="email" name="email" placeholder="Email" required>
-    <input type="submit" name="Reset" value="Reset">
-</form>
+<head>
+    <meta charset="utf-8">
+    <title> Forgot Your Password? </title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/doctor_style/forgot_password.css') }}">
+</head>
+
+<body>
+    <form class="box2" action="{{ route('password.update') }}" method="POST">
+        @csrf
+        <h2> Forgot password </h2>
+        <p class="message"> Enter your email address to reset your password. You may need to check your email </p>
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="submit" name="Reset" value="Reset">
+    </form>
+</body>
+
+</html>
